ce1c8c44e292da5ed814a0da9147f7ae68ae5e9b
Michael Strobel <mstrobel97@gmail.com>
2024-09-06 10:10:43 +0200

    feat: use kanban plugin

--- nvim/plugin/kanban.lua
+++ nvim/plugin/kanban.lua
@@ -4,6 +4,20 @@ end
 
 vim.g.loaded_plugin_kanban = 1
 
-vim.keymap.set("n", "<Leader>k", function()
-  require("me.kanban").toggle()
-end, { desc = "Kanban" })
+---@type kanban.config
+vim.g.kanban = {
+  sources = {
+    {
+      type = "gitlab",
+      name = "todo",
+      data = {
+        token = "GITLAB_ACCESS_TOKEN",
+        project = "GITLAB_PROJECT_URL",
+        boardId = 50,
+      },
+      initial_focus = function()
+        return ({ "Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag" })[tonumber(os.date "%w")]
+      end,
+    },
+  },
+}
