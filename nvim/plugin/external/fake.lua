325095b79f20be5b07d84c7253d7fb2f617a4d84
Michael Strobel <mstrobel97@gmail.com>
2024-11-06 13:39:08 +0100

    feat(fake.nvim): add codelenses

--- nvim/plugin/external/fake.lua
+++ nvim/plugin/external/fake.lua
@@ -27,4 +27,74 @@ vim.g.fake = {
       },
     },
   },
+  codelenses = {
+    {
+      enabled = function(buf)
+        return vim.bo[buf].filetype == "nix" and vim.uri_from_bufnr(buf):match "flake%.nix"
+      end,
+      lenses = function(buf)
+        local parser = vim.treesitter.get_parser(buf, "nix")
+
+        if not parser then
+          return
+        end
+
+        local query = vim.treesitter.query.parse(
+          "nix",
+          [[
+            (binding
+              attrpath: (attrpath
+                attr: (identifier) @_identifier
+                (#eq? @_identifier "url")
+              )
+              expression: (string_expression
+                (string_fragment) @url
+              )
+            )
+          ]]
+        )
+
+        local urls = {}
+
+        for _, match in query:iter_matches(parser:trees()[1]:root(), buf, 0, -1) do
+          for id, nodes in pairs(match) do
+            local name = query.captures[id]
+            if name == "url" then
+              for _, node in ipairs(nodes) do
+                local sline, srow, eline, erow = vim.treesitter.get_node_range(node)
+                local url = vim.treesitter.get_node_text(node, buf)
+
+                local split = vim.split(url, "/")
+
+                ---@type lsp.CodeLens
+                local lens = {
+                  range = {
+                    start = {
+                      line = sline,
+                      character = srow,
+                    },
+                    ["end"] = {
+                      line = eline,
+                      character = erow,
+                    },
+                  },
+                  command = {
+                    title = "open",
+                    command = "open_url",
+                    arguments = {
+                      url = split[1]:gsub("github%:", "https://github.com/") .. "/" .. split[2],
+                    },
+                  },
+                }
+
+                table.insert(urls, lens)
+              end
+            end
+          end
+        end
+
+        return urls
+      end,
+    },
+  },
 }
