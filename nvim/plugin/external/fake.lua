9ecb5443aea53a1882ff15d8259d9bf104c48069
Michael Strobel <mstrobel97@gmail.com>
2024-11-06 13:38:03 +0100

    feat(fake.nvim): update configuration

--- nvim/plugin/external/fake.lua
+++ nvim/plugin/external/fake.lua
@@ -7,15 +7,22 @@ vim.g.loaded_plugin_fake = 1
 ---@type fake.config
 vim.g.fake = {
   snippets = {
-    filetypes = {
-      php = {
+    {
+      enabled = function(buf)
+        return vim.bo[buf].filetype == "php"
+      end,
+      snippets = {
         debug = "Util::getLogger()->debug($0);",
         getset = function()
           return "public function get$2(): $3 {\n\treturn \\$this->$1;\n}\n\npublic function set$2($3 \\$$1): void {\n\t\\$this->$1 = \\$$1;\n}"
         end,
       },
-
-      javascript = {
+    },
+    {
+      enabled = function(buf)
+        return vim.bo[buf].filetype == "javascript"
+      end,
+      snippets = {
         log = "console.log($0);",
       },
     },
