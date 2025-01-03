7f342105154f57df22364b1540c21e2e44fe3dee
Michael Strobel <mstrobel97@gmail.com>
2025-01-03 22:33:16 +0100

    feat(starter): remove VIMRUNTIME item

--- nvim/plugin/external/starter.lua
+++ nvim/plugin/external/starter.lua
@@ -7,25 +7,13 @@ vim.g.loaded_plugin_starter = 1
 ---@type starter.config
 vim.g.starter = {
   items = function()
-    return vim.list_extend(
-      {
-        {
-          text = "source",
-          action = function()
-            vim.cmd "silent! %bwipeout"
-            vim.cmd.cd(vim.env.VIMRUNTIME)
-            require("mini.pick").registry.files()
-          end,
-        },
-      },
-      vim.tbl_map(function(session)
-        return {
-          text = session,
-          action = function()
-            require("session").load(session)
-          end,
-        }
-      end, require("session").list())
-    )
+    return vim.tbl_map(function(session)
+      return {
+        text = session,
+        action = function()
+          require("session").load(session)
+        end,
+      }
+    end, require("session").list())
   end,
 }
