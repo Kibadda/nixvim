c38d0a8d009dfa88b632718502f1e53e3472687d
Michael Strobel <mstrobel97@gmail.com>
2025-05-21 11:54:57 +0200

    refactor(lsp): index table by method instead of each mapping

--- nvim/lua/me/lsp/attach.lua
+++ nvim/lua/me/lsp/attach.lua
@@ -69,80 +69,85 @@ autocmd("LspAttach", {
 
     local methods = vim.lsp.protocol.Methods
 
-    ---@type table<string, { method?: string, lhs?: string, rhs?: function, mode?: string, desc?: string, extra?: function }>
+    ---@type table<string, ({ lhs: string, rhs: function, desc: string, mode?: string|string[] }|function)[]>
     local maps = {
-      {
-        method = methods.textDocument_definition,
-        lhs = "gd",
-        rhs = function()
-          vim.lsp.buf.definition { on_list = on_list }
-        end,
-        desc = "Definition",
-      },
-      {
-        method = methods.textDocument_definition,
-        lhs = "gD",
-        rhs = function()
-          local params = vim.lsp.util.make_position_params(0, client.offset_encoding)
-          vim.lsp.buf_request(bufnr, vim.lsp.protocol.Methods.textDocument_definition, params, function(_, result)
-            if not result or vim.tbl_isempty(result) then
-              return nil
-            end
-            local buf = vim.lsp.util.preview_location(result[1], {})
-            if buf then
-              local cur_buf = vim.api.nvim_get_current_buf()
-              local filetype = vim.bo[cur_buf].filetype
-              if filetype == "php" then
-                filetype = "php_only"
+      [methods.textDocument_definition] = {
+        {
+          lhs = "gd",
+          rhs = function()
+            vim.lsp.buf.definition { on_list = on_list }
+          end,
+          desc = "Definition",
+        },
+        {
+          lhs = "gD",
+          rhs = function()
+            local params = vim.lsp.util.make_position_params(0, client.offset_encoding)
+            vim.lsp.buf_request(bufnr, vim.lsp.protocol.Methods.textDocument_definition, params, function(_, result)
+              if not result or vim.tbl_isempty(result) then
+                return nil
               end
-              vim.bo[buf].filetype = filetype
-            end
-          end)
-        end,
-        desc = "Peek definition",
+              local buf = vim.lsp.util.preview_location(result[1], {})
+              if buf then
+                local cur_buf = vim.api.nvim_get_current_buf()
+                local filetype = vim.bo[cur_buf].filetype
+                if filetype == "php" then
+                  filetype = "php_only"
+                end
+                vim.bo[buf].filetype = filetype
+              end
+            end)
+          end,
+          desc = "Peek definition",
+        },
       },
-      {
-        method = methods.textDocument_references,
-        lhs = "grr",
-        rhs = function()
-          vim.lsp.buf.references({
-            includeDeclaration = false,
-          }, { on_list = on_list })
-        end,
-        desc = "References",
+      [methods.textDocument_references] = {
+        {
+          lhs = "grr",
+          rhs = function()
+            vim.lsp.buf.references({
+              includeDeclaration = false,
+            }, { on_list = on_list })
+          end,
+          desc = "References",
+        },
       },
-      {
-        method = methods.textDocument_implementation,
-        lhs = "gI",
-        rhs = function()
-          vim.lsp.buf.implementation { on_list = on_list }
-        end,
-        desc = "Implementations",
+      [methods.textDocument_implementation] = {
+        {
+          lhs = "gI",
+          rhs = function()
+            vim.lsp.buf.implementation { on_list = on_list }
+          end,
+          desc = "Implementations",
+        },
       },
-      {
-        method = methods.textDocument_documentSymbol,
-        lhs = "gs",
-        rhs = function()
-          vim.lsp.buf.document_symbol { on_list = on_list }
-        end,
-        desc = "Document Symbols",
+      [methods.textDocument_documentSymbol] = {
+        {
+          lhs = "gs",
+          rhs = function()
+            vim.lsp.buf.document_symbol { on_list = on_list }
+          end,
+          desc = "Document Symbols",
+        },
       },
-      {
-        method = methods.workspace_symbol,
-        lhs = "gS",
-        rhs = function()
-          vim.lsp.buf.workspace_symbol(nil, { on_list = on_list })
-        end,
-        desc = "Workspace Symbols",
+      [methods.workspace_symbol] = {
+        {
+          lhs = "gS",
+          rhs = function()
+            vim.lsp.buf.workspace_symbol(nil, { on_list = on_list })
+          end,
+          desc = "Workspace Symbols",
+        },
       },
-      {
-        method = methods.textDocument_codeLens,
-        lhs = "grl",
-        rhs = function()
-          vim.lsp.codelens.run()
-        end,
-        desc = "Run Codelens",
-        extra = function()
+      [methods.textDocument_codeLens] = {
+        {
+          lhs = "grl",
+          rhs = function()
+            vim.lsp.codelens.run()
+          end,
+          desc = "Run Codelens",
+        },
+        function()
           clear { group = groups.codelens, buffer = bufnr }
           autocmd({ "BufEnter", "CursorHold", "InsertLeave" }, {
             group = groups.codelens,
@@ -153,9 +158,8 @@ autocmd("LspAttach", {
           })
         end,
       },
-      {
-        method = methods.textDocument_documentHighlight,
-        extra = function()
+      [methods.textDocument_documentHighlight] = {
+        function()
           clear { group = groups.highlight, buffer = bufnr }
           autocmd({ "CursorHold", "InsertLeave", "BufEnter" }, {
             group = groups.highlight,
@@ -169,32 +173,30 @@ autocmd("LspAttach", {
           })
         end,
       },
-      {
-        method = methods.textDocument_inlayHint,
-        lhs = "gri",
-        rhs = function()
-          vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = bufnr }, { bufnr = bufnr })
-        end,
-        desc = "Toggle Inlay Hint",
+      [methods.textDocument_inlayHint] = {
+        {
+          lhs = "gri",
+          rhs = function()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = bufnr }, { bufnr = bufnr })
+          end,
+          desc = "Toggle Inlay Hint",
+        },
       },
-      {
-        method = methods.textDocument_documentColor,
-        extra = function()
+      [methods.textDocument_documentColor] = {
+        function()
           require("mini.hipatterns").disable(bufnr)
           vim.lsp.document_color.enable(true, bufnr)
         end,
       },
-      {
-        method = methods.textDocument_formatting,
-        extra = function()
+      [methods.textDocument_formatting] = {
+        function()
           if not vim.b[bufnr].formatter then
             vim.b[bufnr].formatter = vim.lsp.buf.format
           end
         end,
       },
-      {
-        method = methods.textDocument_completion,
-        extra = function()
+      [methods.textDocument_completion] = {
+        function()
           vim.lsp.completion.enable(true, client.id, bufnr, {
             autotrigger = true,
             convert = function(item)
@@ -216,109 +218,107 @@ autocmd("LspAttach", {
             end,
           })
         end,
-      },
-      {
-        method = methods.textDocument_completion,
-        mode = "i",
-        lhs = "<C-Space>",
-        rhs = function()
-          vim.lsp.completion.get()
-        end,
-      },
-      {
-        method = methods.textDocument_completion,
-        mode = "i",
-        lhs = "<CR>",
-        rhs = function()
-          local keys
-          if vim.fn.pumvisible() == 1 then
-            local info = vim.fn.complete_info()
-
-            if info.selected == -1 or not should_confirm then
-              keys = vim.api.nvim_replace_termcodes("<C-e>", true, false, true)
-                .. require("nvim-autopairs").autopairs_cr()
-            else
-              keys = vim.api.nvim_replace_termcodes("<C-y>", true, false, true)
-            end
-          else
-            keys = require("nvim-autopairs").autopairs_cr()
-          end
-
-          vim.api.nvim_feedkeys(keys, "n", false)
-        end,
-      },
-      {
-        method = methods.textDocument_completion,
-        mode = { "i", "s" },
-        lhs = "<Tab>",
-        rhs = function()
-          local function has_words_before()
-            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-            return col ~= 0 and not vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s"
-          end
-
-          local keys
-          if vim.fn.pumvisible() == 1 then
-            should_confirm = true
-            keys = vim.api.nvim_replace_termcodes("<C-n>", true, false, true)
-          elseif has_words_before() then
+        {
+          mode = "i",
+          lhs = "<C-Space>",
+          rhs = function()
             vim.lsp.completion.get()
-          else
-            keys = vim.api.nvim_replace_termcodes("<Tab>", true, false, true)
-          end
+          end,
+        },
+        {
+          mode = "i",
+          lhs = "<CR>",
+          rhs = function()
+            local keys
+            if vim.fn.pumvisible() == 1 then
+              local info = vim.fn.complete_info()
 
-          if keys then
+              if info.selected == -1 or not should_confirm then
+                keys = vim.api.nvim_replace_termcodes("<C-e>", true, false, true)
+                  .. require("nvim-autopairs").autopairs_cr()
+              else
+                keys = vim.api.nvim_replace_termcodes("<C-y>", true, false, true)
+              end
+            else
+              keys = require("nvim-autopairs").autopairs_cr()
+            end
+
             vim.api.nvim_feedkeys(keys, "n", false)
-          end
-        end,
-      },
-      {
-        method = methods.textDocument_completion,
-        mode = { "i", "s" },
-        lhs = "<S-Tab>",
-        rhs = function()
-          local keys
-          if vim.fn.pumvisible() == 1 then
-            should_confirm = true
-            keys = vim.api.nvim_replace_termcodes("<C-p>", true, false, true)
-          else
-            keys = vim.api.nvim_replace_termcodes("<S-Tab>", true, false, true)
-          end
+          end,
+        },
+        {
+          mode = { "i", "s" },
+          lhs = "<Tab>",
+          rhs = function()
+            local function has_words_before()
+              local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+              return col ~= 0 and not vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s"
+            end
 
-          vim.api.nvim_feedkeys(keys, "n", false)
-        end,
+            local keys
+            if vim.fn.pumvisible() == 1 then
+              should_confirm = true
+              keys = vim.api.nvim_replace_termcodes("<C-n>", true, false, true)
+            elseif has_words_before() then
+              vim.lsp.completion.get()
+            else
+              keys = vim.api.nvim_replace_termcodes("<Tab>", true, false, true)
+            end
+
+            if keys then
+              vim.api.nvim_feedkeys(keys, "n", false)
+            end
+          end,
+        },
+        {
+          mode = { "i", "s" },
+          lhs = "<S-Tab>",
+          rhs = function()
+            local keys
+            if vim.fn.pumvisible() == 1 then
+              should_confirm = true
+              keys = vim.api.nvim_replace_termcodes("<C-p>", true, false, true)
+            else
+              keys = vim.api.nvim_replace_termcodes("<S-Tab>", true, false, true)
+            end
+
+            vim.api.nvim_feedkeys(keys, "n", false)
+          end,
+        },
       },
-      {
-        mode = { "i", "s" },
-        lhs = "<C-l>",
-        rhs = function()
-          if vim.snippet.active { direction = 1 } then
-            vim.snippet.jump(1)
-          end
-        end,
-      },
-      {
-        mode = { "i", "s" },
-        lhs = "<C-h>",
-        rhs = function()
-          if vim.snippet.active { direction = -1 } then
-            vim.snippet.jump(-1)
-          end
-        end,
+      _ = {
+        {
+          mode = { "i", "s" },
+          lhs = "<C-l>",
+          rhs = function()
+            if vim.snippet.active { direction = 1 } then
+              vim.snippet.jump(1)
+            end
+          end,
+        },
+        {
+          mode = { "i", "s" },
+          lhs = "<C-h>",
+          rhs = function()
+            if vim.snippet.active { direction = -1 } then
+              vim.snippet.jump(-1)
+            end
+          end,
+        },
       },
     }
 
-    for _, mapping in ipairs(maps) do
-      if not mapping.method or client:supports_method(mapping.method) then
-        if mapping.lhs then
-          vim.keymap.set(mapping.mode or "n", mapping.lhs, mapping.rhs, {
-            buffer = bufnr,
-            desc = mapping.desc,
-          })
-        end
-
-        if mapping.extra then
-          mapping.extra()
+    for method, mappings in pairs(maps) do
+      if method == "_" or client:supports_method(method) then
+        for _, mapping in ipairs(mappings) do
+          if type(mapping) == "table" then
+            vim.keymap.set(mapping.mode or "n", mapping.lhs, mapping.rhs, {
+              buffer = bufnr,
+              desc = mapping.desc,
+            })
+          else
+            mapping()
+          end
         end
       end
     end
